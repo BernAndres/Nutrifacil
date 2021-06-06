029d727d3dc51f0e3e36e598cffa5f69ed68db06
Bernandres <69559600+BernAndres@users.noreply.github.com>
2021-06-06 01:34:38 -0400

    Update datosNombre.jsx

--- src/datosNombre.jsx
+++ src/datosNombre.jsx
@@ -13,6 +13,9 @@ export const dataSaludarUser = async function() {
     docRef.onSnapshot(function(doc) {
       const callId = document.getElementById("idSaludarUser");
       if (doc && doc.exists) {
-        callId.innerText = "Bienvenido " + doc.data().Nombre;
+        callId.innerText = "Bienvenido " + doc.data().Nombre1;
+         //aqui se está solicitando Nombre1 que contiene "Juan"
+        //también esta Nombre2 que contiene un number 0, para
+        //probar la diferencia entre un dato lleno y otro que no quiero usar.
       }}
       );}
